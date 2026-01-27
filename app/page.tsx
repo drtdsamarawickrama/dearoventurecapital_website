dbef59699777e363c603680a2df5535f58e0a7a6
Denuwan <sandeepadenuwan980@gmail.com>
2026-01-27 10:58:28 +0530

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Stats from "@/components/Stats/Stats";
 import AboutSection from "@/components/About/AboutSection";
 
 import SectorsSection from "@/components/Sectors/SectorsSection";
-import AwardsSection from "@/components/Awards/AwardsSection";
-import TestimonialsSection from "@/components/Testimonials/TestimonialsSection";
+
+
 import ContactSection from "@/components/Contact/ContactSection";
 import CommunitySection from "@/components/Community/CommunitySection";
 import NewsSection from "@/components/News/NewsSection";
