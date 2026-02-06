adcb583e7d72c93a4e7ff3f9d6a9ec10afb3b16a
Denuwan <sandeepadenuwan980@gmail.com>
2026-02-06 15:18:58 +0530

    update db

--- lib/mongodb.ts
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from "mongodb";
-
-if (!process.env.MONGODB_URI) throw new Error("Add Mongo URI to .env.local");
-
-const client = new MongoClient(process.env.MONGODB_URI);
-const clientPromise = client.connect();
-
-export default clientPromise;
