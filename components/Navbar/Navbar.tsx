3706e7d3c32dff388f219cd20c972a225c446857
rasinis <rashinisewwandi93@gmail.com>
2026-08-21 16:37:51 +0530

    update nav bar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -164,7 +164,7 @@ export default function Navbar() {
                   className="nav-link"
                   onClick={closeMenu}
                 >
-                  Establish Finance
+                  Islamic Finance
                 </Link>
               </li>
 
