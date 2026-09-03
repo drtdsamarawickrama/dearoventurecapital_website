59f5b6a32c7b992fa31832916afd423e0c134e06
rasinis <rasinisewwandi97@gmail.com>
2026-09-03 10:03:40 +0530

    mod: fc investors

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -309,8 +309,8 @@ export default function Navbar() {
               {/* {/* =================================================
                   Apply Now
               ================================================= */}
-
-              {/* { <li className="nav-item">
+{/* 
+               { <li className="nav-item">
                 <Link
                   href="/portal"
                   className="nav-link"
@@ -318,7 +318,7 @@ export default function Navbar() {
                 >
                   Apply Now
                 </Link>
-              </li> }  */} 
+              </li> }   */}
 
             </ul>
           </div>
